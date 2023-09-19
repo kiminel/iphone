9aecdaa8acb1887d0f69e9144082817241c24308
Kimi Nel <kimi.monique.nel@gmail.com>
2023-09-19 16:06:47 +0200

    feat: iPhone Concept Website

---
+++ README.md
@@ -0,0 +1,26 @@
+# iPhone Concept Website 📱
+
+### Hosted Link:
+
+[iPhone](https://www.google.com)
+
+### Local Startup
+
+Run the following commands to spin up locally:
+
+    npm i
+    npm run build
+    npm run dev
+
+## About:
+
+An iPhone concept website incorporating movement and 3D animations. Inspired by the [Apple website](https://www.apple.com/za/iphone-14-pro/) and [iPhone 14 Pro specs](https://www.apple.com/za/iphone-14-pro/specs/).
+
+## Tech Stack
+
+- [React](https://react.dev/)
+- [Vite](https://vitejs.dev/)
+- [Three.js](https://threejs.org/)
+- [WebGi SDK](https://webgi.xyz/)
+- [GSAP](https://greensock.com/gsap/)
+- [Sketchfab](https://sketchfab.com/3d-models/iphone-13-pro-max-4f92b60d824a42c89bbf1833374c4f73)
---
+++ src/components/DisplaySection.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const DisplaySection = ({ triggerPreview }) => {
+  const handleScrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <div className="display-section wrapper">
+      <h2 className="title">Always-On display</h2>
+      <p className="text">Super Retina</p>
+
+      <span className="description">
+        The XDR display is 2x brighter in the sun.
+      </span>
+
+      <button className="button" onClick={triggerPreview}>
+        Try me!
+      </button>
+      <button className="back-button" onClick={handleScrollToTop}>
+        TOP
+      </button>
+    </div>
+  );
+};
+
+export default DisplaySection;
